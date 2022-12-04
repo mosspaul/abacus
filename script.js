b2b283686addaa38e25d1562a30a4534b9f054d1
mosspaul <gmosspaul18@gmail.com>
2022-12-04 15:10:37 -0600

    Add outline for future

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ for (let i = 0; i < 17; i++) {
     makeRod(ONES);
     makeRod(FIVER);
 }
+
+/*
+Here I'll add the beads functionality
+
+1. Build the beads programatically
+2. Separate the fives and ones and give them the respective values
+3. Make a resetter that resets the count and the ui
+4. Have a rolling count that tracks the numbers
+*/

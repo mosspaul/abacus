c13e75996d70e9d9dec233dcd4d9603a127df971
mosspaul <gmosspaul18@gmail.com>
2022-12-08 18:52:54 -0600

    Created initial bead functionality and styling

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const FIVER = document.querySelector(".fiver");
 const ONES = document.querySelector(".ones");
 
-function makeRod(parentNode) {
-    let rod = document.createElement("div");
-    rod.setAttribute("class", "rod");
-    parentNode.appendChild(rod);
+
+function makeDiv(parentNode, cssClass, returnable) {
+    let div = document.createElement("div");
+    div.setAttribute("class", cssClass);
+    parentNode.appendChild(div);
+    if (returnable === true) {
+        return parentNode.appendChild(div);
+    }
 }
 
 for (let i = 0; i < 17; i++) {
-    makeRod(ONES);
-    makeRod(FIVER);
+    makeDiv(ONES, "rod", false);
+    makeDiv(FIVER, "rod", false);
 }
-
+const FIVER_ROD = document.querySelector(".fiver > .rod");
+const ONES_ROD = document.querySelector(".ones > .rod");
 /*
 Here I'll add the beads functionality
 
@@ -20,3 +25,28 @@ Here I'll add the beads functionality
 3. Make a resetter that resets the count and the ui
 4. Have a rolling count that tracks the numbers
 */
+
+let counter = {
+    value: 0,
+    element: document.querySelector("p"),
+}
+
+
+function Bead(value, cssClass, parentNode, element) {
+    this.value = value;
+    this.cssClass = cssClass,
+    this.parentNode = parentNode,
+    this.element = element
+}
+
+let fiverBead = new Bead(
+    value = 5, 
+    cssClass = "bead",
+    parentNode = FIVER_ROD,   
+    element = makeDiv(this.parentNode, this.cssClass, true),
+);
+
+fiverBead.element.addEventListener("click", () => {
+    fiverBead.element.parentNode.style.justifyContent = "end";
+    counter.element.textContent = `Count: ${counter.value += fiverBead.value}`;
+});
